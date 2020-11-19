2bc71acf3b999968fe89b13aff4f629409bc7469
Nihar Sahu <sahunihar0602@gmail.com>
2020-11-19 21:11:09 +0530

    Slider React Project using State, Effect Hooks and Conditional Rendering.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
 function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  const checkIndex = (i) => {
+    if (i < 0){
+      return people.length - 1; 
+    }
+    if (i > people.length - 1){
+      return 0;
+    }
+    return i; 
+  }
+
+  const prevButton = () => {
+    setIndex((prevIndex) => {
+      const newIndex = prevIndex - 1;
+      return checkIndex(newIndex);
+    })
+  }
+
+  const nextButton = () => {
+    setIndex((nextIndex) => {
+      const newIndex  = nextIndex + 1;
+      return checkIndex(newIndex);
+    })
+  }
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(checkIndex(index + 1))
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [index])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='section'>
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {
+          people.map((person, personIndex) => {
+            const { id, image, name, title, quote } = person;
+            let position = 'nextSlide'
+            if(personIndex === index) {
+              position = 'activeSlide';
+            }
+            if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)){
+              position = 'lastSlide';
+            }
+            return (
+              <article key={id} className={position}>
+                <img src={image} alt={name} className='person-img'/>
+                <h4>{name}</h4>
+                <p className='title'>{title}</p>
+                <p className="text">{quote}</p>
+                <FaQuoteRight className="icon" />
+              </article>
+            );
+          })
+        }
+        <button className='prev' onClick={prevButton}>
+          <FiChevronLeft/>
+        </button>
+        <button className='next' onClick={nextButton}>
+          <FiChevronRight/>
+        </button>
+      </div>
+    </section>
   );
 }
 
